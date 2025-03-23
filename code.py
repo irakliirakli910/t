7171d6841f0f1db803bade0ea4c9d687ef43d4b8
irakliirakli910 <irakliirakli910@gmail.com>
2025-03-23 13:42:17 +0400

    daemata meore

--- code.py
+++ code.py
@@ -1,1 +1,13 @@
+def counter(s,e):
+    i=s
+    while i<=e:
+        yield i
+        i+=1
+        print("sds",i)
+for i in counter(0,10):
+    print(i)
 print("hello")
+gen = [x for x in range(5)]
+print(gen)
+for num in gen:
+    print(num)
